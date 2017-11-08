9cd8b9242ea8f471b124f1b3fd5228d01ef57942
Jonathon Kelly <jonathon@insidersbyte.com>
2017-11-08 15:39:38 +0000

    :sparkles: generate bindings

---
+++ scripts/generate-bindings.js
@@ -0,0 +1,17 @@
+const icons = require('material-ui-icons');
+const fs = require('fs');
+const os = require('os');
+
+const contents = Object.keys(icons)
+  .map(
+    icon => `module ${icon} = {
+  [@bs.module "material-ui-icons/${
+    icon
+  }"] external reactClass : ReasonReact.reactClass = "default";
+  let make = (children) =>
+    ReasonReact.wrapJsForReason(~reactClass, ~props=Js.Obj.empty(), children);
+};`,
+  )
+  .join(`${os.EOL}${os.EOL}`);
+
+fs.writeFileSync('./src/MaterialUIIcons.re', contents);
